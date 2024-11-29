6664600e544b2d1c366ec07693f2e471f74cbaf2
RussianGamer1050 <Russian.Gaimer@yandex.ru>
2024-11-30 01:13:20 +0300

    Added raw interface

--- work_time_tracker/src/main/java/com/javafx/WorkTimeTracker.java
+++ work_time_tracker/src/main/java/com/javafx/WorkTimeTracker.java
@@ -1,5 +1,42 @@
 package com.javafx;
 
-public class WorkTimeTracker {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class WorkTimeTracker extends Application {
     
+    @Override
+    public void start(Stage stage) {
+
+        // Create labels for display
+        Label workingTimeLabel = new Label("Total Working Time: 00:00:00");
+        Label breakTimeLabel = new Label("Break Time: 00:00:00");
+        VBox labelsVBox = new VBox(15, workingTimeLabel, breakTimeLabel);
+
+        // Buttons to control the application
+        Button startWorkButton = new Button("Start Work");
+        Button takeBreakButton = new Button("Take Break");
+        Button continueWorkingButton = new Button("Continue Working");
+        Button endWorkButton = new Button("End Work");
+        VBox buttonsVBox = new VBox(10, startWorkButton, takeBreakButton, continueWorkingButton, endWorkButton);
+
+        // Layout setup
+        VBox root = new VBox(30, labelsVBox, buttonsVBox);
+        root.setPadding(new Insets(20));
+
+        // Scene and stage setup
+        Scene scene = new Scene(root, 200, 270);
+        stage.setTitle("Work Time Tracker");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- work_time_tracker/src/main/java/module-info.java
+++ work_time_tracker/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.javafx {
     requires javafx.graphics;
+    requires javafx.controls;
 
     opens com.javafx to javafx.graphics;
 }
