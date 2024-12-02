8eee75c3789f44c6e3a5bbca3fb07c68e0e42272
RussianGamer1050 <Russian.Gaimer@yandex.ru>
2024-12-02 23:50:57 +0300

    Added button actions and test timeline

--- work_time_tracker/src/main/java/com/javafx/WorkTimeTracker.java
+++ work_time_tracker/src/main/java/com/javafx/WorkTimeTracker.java
@@ -1,5 +1,7 @@
 package com.javafx;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
@@ -7,12 +9,22 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class WorkTimeTracker extends Application {
     
+    // Status flags
+    private boolean isWorking = false;
+    private boolean isOnBreak = false;
+
+    private void update() {
+        System.out.println("isWorking: " + isWorking + " | isOnBreak: " + isOnBreak);
+    }
+
     @Override
     public void start(Stage stage) {
 
+        // Interface elements
         // Create labels for display
         Label workingTimeLabel = new Label("Total Working Time: 00:00:00");
         Label breakTimeLabel = new Label("Break Time: 00:00:00");
@@ -34,6 +46,37 @@ public class WorkTimeTracker extends Application {
         stage.setTitle("Work Time Tracker");
         stage.setScene(scene);
         stage.show();
+
+        // Button actions
+        // Start button action
+        startWorkButton.setOnAction(event -> {
+            isWorking = true;
+        });
+
+        // Take break button action
+        takeBreakButton.setOnAction(event -> {
+            isWorking = false;
+            isOnBreak = true;
+        });
+
+        // Continue working button action
+        continueWorkingButton.setOnAction(event -> {
+            isOnBreak = false;
+            isWorking = true;
+        });
+
+        // End work button action
+        endWorkButton.setOnAction(event -> {
+            isWorking = false;
+            isOnBreak = false;
+        });
+
+        // Timeline which update time every millisecond
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(1), event -> update());
+        Timeline timeline = new Timeline(keyFrame);
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
+
     }
 
     public static void main(String[] args) {
